9a594f51e03a489b3fbb3d5990ed15513ba057bf
zhangyu <zhangyu9069@shein.com>
2020-03-24 19:48:39 +0800

    commit

---
+++ gradle-thread/src/main/java/com/zhangyu/gradlethread/GradleThreadApplication.java
@@ -0,0 +1,13 @@
+package com.zhangyu.gradlethread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleThreadApplication.class, args);
+    }
+
+}
