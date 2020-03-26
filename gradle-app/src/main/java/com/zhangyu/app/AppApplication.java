a7a100bb1ab5e0725d934ad6251a328cf39c0d06
zhangyu <zhangyu9069@shein.com>
2020-03-26 18:52:09 +0800

    project modify

---
+++ gradle-app/src/main/java/com/zhangyu/app/AppApplication.java
@@ -0,0 +1,17 @@
+package com.zhangyu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangyu
+ * @description
+ * @date 2020/3/26
+ */
+@SpringBootApplication
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+        System.out.println("!!!ojbk!!!");
+    }
+}
