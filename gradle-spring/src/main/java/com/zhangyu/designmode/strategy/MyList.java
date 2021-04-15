515e720fb593a003ffaf8389f108fff7ef008ae9
chao.ding <852611546@qq.com>
2021-04-15 23:16:26 +0800

    原型模式

---
+++ gradle-spring/src/main/java/com/zhangyu/designmode/strategy/MyList.java
@@ -0,0 +1,14 @@
+package com.zhangyu.designmode.strategy;
+
+/**
+ * @author : zhangyu
+ * @description : ${描述}
+ * @date : 2021/4/15
+ */
+public class MyList {
+
+    public void sort (ComparatorIntf c) {
+        c.compare(24 , 2);
+        System.out.println("wqwetqwtqww");
+    }
+}
