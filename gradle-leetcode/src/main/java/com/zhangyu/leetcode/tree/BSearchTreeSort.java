ff73a034a5e45f56e01137c0e448b7971101e049
chao.ding <852611546@qq.com>
2021-05-11 17:57:15 +0800

    二叉查找树-删除

--- gradle-leetcode/src/main/java/com/zhangyu/leetcode/tree/BSearchTreeSort.java
+++ gradle-leetcode/src/main/java/com/zhangyu/leetcode/tree/BSearchTreeSort.java
@@ -17,12 +17,13 @@ public class BSearchTreeSort {
 
     private TreeNode treeNode;
 
-    BSearchTreeSort (TreeNode treeNode) {
+    BSearchTreeSort(TreeNode treeNode) {
         this.treeNode = treeNode;
     }
 
     /**
      * 遍历查找树中值为d的节点
+     *
      * @param d
      */
     public TreeNode find(int d) {
@@ -42,9 +43,10 @@ public class BSearchTreeSort {
 
     /**
      * 插入
+     *
      * @param d
      */
-    public void insert (int d) {
+    public void insert(int d) {
         TreeNode inode = new TreeNode(d);
         TreeNode p = treeNode;
         while (p != null) {
@@ -55,8 +57,7 @@ public class BSearchTreeSort {
                     return;
                 }
                 p = p.getRNode();
-            }
-            else  {
+            } else {
                 if (p.getLNode() == null) {
                     p.setLNode(inode);
                     return;
@@ -66,14 +67,32 @@ public class BSearchTreeSort {
         }
     }
 
+    /**
+     * 删除
+     *
+     * @param d
+     */
+    public void delete(int d) {
+        // 查找需要删除的节点
+
+        // 当前节点为 根节点 -> 直接删除
+
+        // 当前节点 有一个子节点 -> 用子节点替换当前
+
+        // 当前节点 有两个子节点 -> 找到右节点中最小的a，替换当前,然后把a的右节点换到a的位置
+    }
+
     public static void main(String[] args) {
-        LinkedList<Integer> a = new LinkedList(Arrays.asList(6, 3,2,1,null,null,null,4,null,null,8,7,null,null,9,null,null));
+        LinkedList<Integer> a = new LinkedList(Arrays.asList(6, 3, 2, 1, null, null, null, 4, null, null, 8, 7, null,
+                null, 9, null, null));
         TreeNode treeNode = TreeNode.createBinaryTree(a);
         BSearchTreeSort treeSort = new BSearchTreeSort(treeNode);
         // 查找
         TreeNode findNode = treeSort.find(2);
-        System.out.println("节点:"+ findNode.getData() + "  左节点:" + (findNode.getLNode() == null ? "null" : findNode.getLNode().getData()) + "  右节点:" + (findNode.getRNode() == null ? "null" : findNode.getRNode().getData()));
-        // 替换
+        System.out.println("节点:" + findNode.getData() + "  左节点:" + (findNode.getLNode() == null ? "null" :
+                findNode.getLNode().getData()) + "  右节点:" + (findNode.getRNode() == null ? "null" :
+                findNode.getRNode().getData()));
+        // 插入
         treeSort.insert(5);
         System.out.println();
     }
